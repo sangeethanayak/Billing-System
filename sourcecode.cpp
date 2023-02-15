0fb4c156115b10450935647705ee6c2baeb86438
Sangeetha Nayak <sangeethanayak333@gmail.com>
2023-02-15 23:09:06 +0530

    Update sourcecode.cpp

--- sourcecode.cpp
+++ sourcecode.cpp
@@ -2,27 +2,246 @@
 #include <fstream>
 #include <iomanip>
 #include <cstdio>
+#include <string>
+#include <conio.h>
 using namespace std;
 
-class shopping
+void item :: get_item(void)
 {
-private:
-    int pcode;
-    float price;
-    float discount;
-    string pname;
-public:
-    void menu();
-    void admin();
-    void customer();
-    void add();
-    void edit();
-    void rem();
-    void list1();
-    void receipt();
+    cout<<"\t\t\t\t\t\t--------------------------------------------------------------"<<endl;
+     cout << setw (88);
+     cout<<"Add New Product"<<endl;
+     cout<<"\t\t\t\t\t\t--------------------------------------------------------------"<<endl;
+     cout << setw (80);
+     cout<<"Product code of the product: ";
+     cin>>pcode;
+     cout << setw (72);
+     cout<<"Name of the product: ";
+     cin>>name;
+     cout << setw (73);
+     cout<<"Price of the product: ";
+     cin>>price;
+     cout << setw (76);
+     cout<<"Discount of the product: ";
+     cin>>discount;
+     cout<<endl;
+}
+void item :: put_item (void)
+{
 
-};
-void shopping :: menu()
+    cout<<setw(65)<<pcode<<setw(10)<<name<<setw(10)<<price<<setw(12)<<discount<<endl;
+
+}
+item it;
+fstream file;
+
+void item:: add_record()
+{
+    char ch='y';
+    file.open("stock.dat", ios::app | ios::binary);
+    while(ch=='y'||ch=='Y')
+    {
+    it.get_item();
+
+    file.write((char*)&it, sizeof(it));
+    cout<<setw(85);
+    cout<<"Add more records..(y/n)? ";
+    cin>>ch;
+    cout<<endl;
+}
+file.close();
+}
+void item::show_all(void)
+{
+    file.open("stock.dat", ios::in | ios::binary);
+    if(!file)
+    {
+        cout<<setw(85);
+        cout<<"File not found";
+        exit(0);
+    }
+    else
+    {
+        file.read((char*)&it, sizeof(it));
+        cout<<endl;
+        cout<<"\t\t\t\t\t\t--------------------------------------------------------------"<<endl;
+        cout << setw (88);
+        cout<<"Product Details"<<endl;
+        cout<<"\t\t\t\t\t\t--------------------------------------------------------------"<<endl;
+        cout<<setw(67)<<"CODE"<<setw(10)<<"NAME"<<setw(10)<<"PRICE"<<setw(12)<<"DISCOUNT"<<endl;
+        while(! file.eof())
+        {
+
+            it.put_item();
+            file.read((char*)&it, sizeof(it));
+        }
+        cout<<endl;
+    }
+    file.close();
+}
+void item:: show_record(void)
+{
+    int no, flag=0;
+    file.open("stock.dat", ios::in|ios::binary);
+    if(!file)
+    {
+        cout<<setw(85);
+        cout<<"File not found";
+        exit(0);
+    }
+    else
+    {
+        cout<<setw(95);
+        cout<<"Enter Product code to search: ";
+        cin>>no;
+        file.read((char*)&it, sizeof(it));
+        while(!file.eof())
+        {
+            if(no==it.get_code())
+            {
+                flag=1;
+                cout<<endl;
+                cout<<"\t\t\t\t\t\t--------------------------------------------------------------"<<endl;
+                cout << setw (88);
+                cout<<"Product Details"<<endl;
+                cout<<"\t\t\t\t\t\t--------------------------------------------------------------"<<endl;
+                cout<<setw(67)<<"CODE"<<setw(10)<<"NAME"<<setw(10)<<"PRICE"<<setw(12)<<"DISCOUNT"<<endl;
+                it.put_item();
+                break;
+            }
+            file.read((char*)&it, sizeof(it));
+        }
+        if(flag==0)
+        {
+            cout<<endl;
+            cout<<setw(88);
+            cout<<"Product not found!\n";
+            cout<<endl;
+        }
+    }
+    file.close();
+}
+
+void item :: delete_record()
+{
+    int no,flag=0;
+    cout<<setw(95);
+    cout<<"Enter Product code to delete: ";
+    cin>>no;
+    ofstream file2;
+    file2.open("new.dat", ios::out|ios::binary);
+    file.open("stock.dat", ios::in|ios::binary);
+    if(!file)
+    {
+        cout<<setw(85);
+        cout<<"File not found";
+        exit(0);
+    }
+    else{
+        file.read((char*)&it, sizeof(it));
+        while(!file.eof())
+        {
+            if(no!=it.get_code())
+            {
+                flag=1;
+                file2.write((char*)&it, sizeof(it));
+            }
+            file.read((char*)&it, sizeof(it));
+        }
+    }
+
+    file2.close();
+    file.close();
+    remove("stock.dat");
+    rename("new.dat","stock.dat");
+
+}
+
+
+
+
+
+
+void item :: admin()
+{
+    Admin:
+    int choice;
+    cout<<"\t\t\t\t\t\t--------------------------------------------------------------"<<endl;
+    cout << setw (85);
+    cout<<"Admin Page"<<endl;
+    cout<<"\t\t\t\t\t\t--------------------------------------------------------------"<<endl;
+    cout << setw (83);
+    cout<<"1. Add product details"<<endl;
+    cout << setw (87);
+    cout<<"2. Display product details"<<endl;
+    cout << setw (97);
+    cout<<"3. Display particular product detail"<<endl;
+    cout << setw (86);
+    cout<<"4. Modify product details"<<endl;
+    cout << setw (86);
+    cout<<"5. Delete product details"<<endl;
+    cout << setw (84);
+    cout<<"6. Go Back to main menu"<<endl<<endl;
+    cout << setw (93);
+    cout<<"Please enter your option: ";
+    cin>>choice;
+    cout<<endl;
+    switch(choice)
+    {
+    case 1:
+        add_record();
+        break;
+    case 2:
+        show_all();
+        break;
+    case 3:
+        show_record();
+        break;
+    case 4:
+        modify_record();
+        break;
+    case 5:
+        delete_record();
+        break;
+    case 6:
+        menu();
+        break;
+    default:
+        cout<<setw(85);
+        cout<<"Invalid choice";
+    }
+   goto Admin;
+}
+void item :: customer()
+{
+    Customer:
+    int choice;
+    cout<<"\t\t\t\t\t\t--------------------------------------------------------------"<<endl;
+    cout << setw (85);
+    cout<<"Customer Page"<<endl;
+    cout<<"\t\t\t\t\t\t--------------------------------------------------------------"<<endl;
+    cout << setw (85);
+    cout<<"1. Buy product"<<endl;
+    cout<<setw(85);
+    cout<<"2. Go Back"<<endl<<endl;
+    cout << setw (89);
+    cout<<"Enter your option: ";
+    cin>>choice;
+    cout<<endl;
+    switch(choice)
+    {
+    case 1:
+        receipt();
+        break;
+    case 2:
+        menu();
+        break;
+    default:
+        cout<<"Invalid Choice"<<endl;
+    }
+    goto Customer;
+}
+void item :: menu()
 {
     Menu:
     int choice;
@@ -86,164 +305,4 @@ void shopping :: menu()
         }
     }
 goto Menu;
-}
-void shopping :: admin()
-{
-    Admin:
-    int choice;
-    cout<<"\t\t\t\t\t\t--------------------------------------------------------------"<<endl;
-    cout << setw (85);
-    cout<<"Admin Page"<<endl;
-    cout<<"\t\t\t\t\t\t--------------------------------------------------------------"<<endl;
-    cout << setw (90);
-    cout<<"1. Add product details"<<endl;
-    cout << setw (93);
-    cout<<"2. Modify product details"<<endl;
-    cout << setw (93);
-    cout<<"3. Delete product details"<<endl;
-    cout << setw (91);
-    cout<<"4. Go Back to main menu"<<endl<<endl;
-    cout << setw (95);
-    cout<<"Please enter your option: ";
-    cin>>choice;
-    cout<<endl;
-    switch(choice)
-    {
-    case 1:
-        add();
-        break;
-    case 2:
-        edit();
-        break;
-    case 3:
-        rem();
-        break;
-    case 4:
-        menu();
-        break;
-    default:
-        cout<<"Invalid choice";
-    }
-   goto Admin;
-}
-void shopping :: customer()
-{
-    Customer:
-    int choice;
-    cout<<"\t\t\t\t\t\t--------------------------------------------------------------"<<endl;
-    cout << setw (85);
-    cout<<"Customer Page"<<endl;
-    cout<<"\t\t\t\t\t\t--------------------------------------------------------------"<<endl;
-    cout << setw (85);
-    cout<<"1. Buy product"<<endl;
-    cout << setw (81);
-    cout<<"2. Go Back"<<endl<<endl;
-    cout << setw (89);
-    cout<<"Enter your option: ";
-    cin>>choice;
-    cout<<endl;
-    switch(choice)
-    {
-    case 1:
-        receipt();
-        break;
-    case 2:
-        menu();
-        break;
-    default:
-        cout<<"Invalid Choice"<<endl;
-    }
-    goto Customer;
-}
-void shopping :: add()
-{
-
-    Add: fstream data;
-     int c;
-     int token = 0;
-     float p;
-     float d;
-     string n;
-     cout<<"\t\t\t\t\t\t--------------------------------------------------------------"<<endl;
-     cout << setw (88);
-     cout<<"Add New Product"<<endl;
-     cout<<"\t\t\t\t\t\t--------------------------------------------------------------"<<endl;
-     cout << setw (80);
-     cout<<"Product code of the product: ";
-     cin>>pcode;
-     cout << setw (72);
-     cout<<"Name of the product: ";
-     cin>>pname;
-     cout << setw (73);
-     cout<<"Price of the product: ";
-     cin>>price;
-     cout << setw (76);
-     cout<<"Discount of the product: ";
-     cin>>discount;
-     cout<<endl;
-
-     if(!data)
-     {
-         data.open("database.txt",ios::in);
-         data.open("database.txt", ios::app|ios::out);
-         data<<" "<<pcode<<" "<<pname<<" "<<price<<" "<<discount<<"%\n";
-         data.close();
-     }
-     else
-     {
-         data>>c>>n>>p>>d;
-         while(!data.eof())
-           {
-             if(c == pcode)
-             {
-                 token++;
-             }
-             data>>c>>n>>p>>d;
-           }
-        data.close();
-
-     if(token == 1)
-        goto Add;
-     else
-     {
-         data.open("database.txt", ios::app|ios::out);
-         data<<"\t\t\t"<<pcode<<"\t\t\t"<<pname<<"\t\t\t"<<price<<"\t\t\t"<<discount<<"%\n";
-         data.close();
-     }
- }
-  cout << setw (88);
-  cout<<"Record inserted"<<endl<<endl;
-}
-void shopping::rem()
-{
-    int no;
-    cout<<setw(91);
-    cout<<"Enter item code to delete: ";
-    cin>>no;
-    ofstream file2;
-    file2.open("new.dat", ios::out|ios::binary);
-    data.open("database.dat", ios::in|ios::binary);
-    if(!data)
-    {
-        cout<<setw(88);
-        cout<<"File not found";
-        exit(0);
-    }
-    else{
-        data.read((char*)&it, sizeof(it));
-        while(!data.eof())
-        {
-            if(no!=it.get_code())
-            {
-                file2.write((char*)&it, sizeof(it));
-            }
-            data.read((char*)&it, sizeof(it));
-            
-        }
-
-    }
-    file2.close();
-    data.close();
-    remove("database.dat");
-    rename("new.dat","database.dat");
 }
